89d09a7600d49ad5d529e3c37f4d2bc94e9163f8
Freddy Peltier <84007024+Freddy92i@users.noreply.github.com>
2022-06-03 15:09:37 +0200

    Add files via upload

---
+++ logs/log.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace formjdrppe
+{
+    public partial class log : Form
+    {
+        public log()
+        {
+            InitializeComponent();
+        }
+        public static List<AllLogs> List = new List<AllLogs>();
+       
+
+        private async void log_Load(object sender, EventArgs e)
+        {
+            if (List.Count == 0)
+            {
+                string response = await api.getAllAsync("/logs");
+                JavaScriptSerializer JSserializer = new JavaScriptSerializer();
+                List = JSserializer.Deserialize<List<AllLogs>>(response);
+
+            }
+
+            
+            dataGridView1.DataSource = List;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.AllowUserToResizeColumns = true;
+        }
+    }
+}
